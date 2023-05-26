7ead656ce2d24d46ad52d453e792cc0542856c4f
Lindsay Kerr <wrenzenzen@gmail.com>
2023-05-26 11:47:57 +0100

    Fixed some uri request bugs, added uri decoding

--- src/server/api.rs
+++ src/server/api.rs
@@ -1,6 +1,7 @@
 use std::path;
 
 use crate::errors;
+use regex::Regex;
 
 #[derive(Debug)]
 pub enum Query {
@@ -55,36 +56,13 @@ impl Query {
 }
 
 
-
-
-pub fn api(method: String, path: String) -> Option<Query>  {
-
-    
-    
-
-    // API routing goes here
-    match method.as_str() {
-        "GET" => {
-            let mut path_seg_root = Box::new(PathSegment::new(String::from("api"), 0));
-            println!("path: {}", path);
-            get_query_tree(&mut path_seg_root);
-
-            query_from_uri(path, &mut path_seg_root)
-
-            
-        },
-        _ => None,
-    }
-}
-
-
-
 pub struct PathSegment {
     pub value: String,
     pub seg_number: u16,
     pub children_segments: Vec<Box<PathSegment>>,
     pub query: Option<Query>,
 }
+
 impl PathSegment {
     pub fn new(value: String, seg_number: u16) -> PathSegment {
         PathSegment {
@@ -102,7 +80,7 @@ impl PathSegment {
 
     pub fn child_seg_by_value(&mut self, value: String) -> &mut Box<PathSegment> {
      
-        
+       
         let child = PathSegment::new(value.clone(), self.seg_number + 1);
         self.children_segments.push(Box::new(child));
         self.children_segments.last_mut().unwrap()
@@ -128,8 +106,28 @@ impl PathSegment {
 }
 
 
+pub fn api(method: String, path: String) -> Option<Query>  {
 
-fn query_from_uri(uri: String, tree_seg_root: &Box<PathSegment>) -> Option<Query> {
+    // API routing goes here
+    match method.as_str() {
+        "GET" => {
+            let mut path_seg_root = Box::new(PathSegment::new(String::from("api"), 0));
+            println!("path: {}", path);
+            get_query_tree(&mut path_seg_root);
+
+            parse_api_request_to_query(path, &mut path_seg_root)
+            
+        },
+        _ => None,
+    }
+}
+
+// This function uses a tree to validate and parse the api uri resource path
+// It will return a query enum if the uri is valid, otherwise it will return None
+fn parse_api_request_to_query(
+    uri: String, 
+    tree_seg_root: &Box<PathSegment>
+) -> Option<Query> {
 
     let mut uri_segs: Vec<_> = uri.split("/").into_iter().map(|x| String::from(x)).collect();
     let mut index = 0;
@@ -140,7 +138,7 @@ fn query_from_uri(uri: String, tree_seg_root: &Box<PathSegment>) -> Option<Query
         return None;
     }
 
-    // the expected first item should be "", so if that item is all there is then return None
+    // the expected first item should be "", so if there is anything else there return None
     if uri_segs.len() == 1 && uri_segs[0].is_empty() {
         return None;
     }
@@ -168,7 +166,7 @@ fn query_from_uri(uri: String, tree_seg_root: &Box<PathSegment>) -> Option<Query
         // if there is a child segment with the same value as the current uri segment
         // then move to that segment
         if let Some(next) = tree_seg.get_next(uri_segs[index].clone()) {
-            // println!("registered valid path segment {} ", uri_segs[index]);
+            //println!("registered valid path segment {} ", uri_segs[index]);
             
             
             if index +1 < uri_segs.len() {  
@@ -179,14 +177,14 @@ fn query_from_uri(uri: String, tree_seg_root: &Box<PathSegment>) -> Option<Query
             else if let Some(query) = &next.query {
                 return Some(query_from_parsed_variables(query, &variables));
             }
-            else {
-                return None;
-            }   
+
+            return None;
+               
         }
         // if there is a child segment with the value "{}" that indicates a uri parameter
         // then save that value 
-        else if let Some(next) = tree_seg.get_next("{}".to_string()) {
-            // println!("registered variable");
+        if let Some(next) = tree_seg.get_next("{}".to_string()) {
+          
             variables.push(uri_segs[index].clone());
             if index + 1 < uri_segs.len() {
                 tree_seg = next;    
@@ -196,16 +194,27 @@ fn query_from_uri(uri: String, tree_seg_root: &Box<PathSegment>) -> Option<Query
             else if let Some(query) = &next.query {
                 return Some(query_from_parsed_variables(query, &variables));
             }
-            else {
-                return None;
-            }
-        }
-        else if let Some(query) = &tree_seg.query {
-            return Some(query_from_parsed_variables(query, &variables));
-        }
-        else {
+            
             return None;
+            
         }
+        // Knowing there is no child segment, it should also be the case that there
+        // is no longer any uri segments left, if there are no more uri segments then 
+        if index == uri_segs.len() {
+            
+            // see if the current segment has a query associated with it
+            if let Some(query) = &tree_seg.query {
+                
+                return Some(query_from_parsed_variables(query, &variables));
+            }
+            
+            return None;
+            
+        }  
+        // Fall through case, the uri segment is not valid
+        return Some(Query::ApiInvalidUri);
+             
+        
     }
     None
 }
@@ -222,12 +231,20 @@ fn query_from_parsed_variables(query: &Query, variables: &Vec<String>) -> Query 
     match query {
         Query::GetStockSupplierIdFromName(_) => {
             let name = &variables[0];
+            let name = uri_seg_decode(name);
             response_query = Query::GetStockSupplierIdFromName(name.clone());
         },
         Query::GETStockSupplierFromId(_) => {
                 if let Ok(id) = variables[0].parse::<u64>() {
                     response_query = Query::GETStockSupplierFromId(id);
                 }    
+        },
+        Query::GetStockSuppliersEmail => {
+            response_query = Query::GetStockSuppliersEmail;
+                
+        },
+        Query::GetStockSuppliersNumbers => {
+            response_query = Query::GetStockSuppliersNumbers;
         },
         Query::GETStockSuppliers => {
             response_query = Query::GETStockSuppliers;
@@ -241,33 +258,58 @@ fn query_from_parsed_variables(query: &Query, variables: &Vec<String>) -> Query 
 fn get_query_tree(path_seg_root: &mut PathSegment)  {
    
    
-        // GET /api/suppliers branch
-        let suppliers = path_seg_root.child_seg_by_value(String::from("suppliers"));
-        suppliers.query = Some(Query::GETStockSuppliers);
+    // **GET /api/suppliers query/branch
+    let suppliers = path_seg_root.child_seg_by_value(String::from("suppliers"));
+    suppliers.query = Some(Query::GETStockSuppliers);
 
-            let mut suppliers_email = suppliers.child_seg_by_value(String::from("email"));
-            suppliers_email.query = Some(Query::GetStockSuppliersEmail);
+        // **GET /api/suppliers/email query
+        let mut suppliers_email = suppliers.child_seg_by_value(String::from("email"));
+        suppliers_email.query = Some(Query::GetStockSuppliersEmail);
 
-            let mut suppliers_numbers = suppliers.child_seg_by_value(String::from("numbers"));
-            suppliers_numbers.query = Some(Query::GetStockSuppliersNumbers);
+        // **GET /api/suppliers/numbers query
+        let mut suppliers_numbers = suppliers.child_seg_by_value(String::from("numbers"));
+        suppliers_numbers.query = Some(Query::GetStockSuppliersNumbers);
 
-        
-        // GET /api/supplier branch
-        let mut supplier = path_seg_root.child_seg_by_value(String::from("supplier"));
+    
+    // GET /api/supplier branch
+    let mut supplier = path_seg_root.child_seg_by_value(String::from("supplier"));
 
-            // GET /api/supplier/id branch
-            let mut supplier_id_seg = supplier.child_seg_by_value(String::from("id"));
-                
-                // GET /api/supplier/id/{name} query
-                let mut supplier_id_name = supplier_id_seg.child_seg_by_value(String::from("{}"));
-                    supplier_id_name.query = Some(Query::GetStockSupplierIdFromName(String::from("{}")));
+        // GET /api/supplier/id branch
+        let mut supplier_id_seg = supplier.child_seg_by_value(String::from("id"));
+            
+            // GET /api/supplier/id/{name} query
+            let mut supplier_id_name = supplier_id_seg.child_seg_by_value(String::from("{}"));
+                supplier_id_name.query = Some(Query::GetStockSupplierIdFromName(String::from("{}")));
 
-            // GET /api/supplier/{id} query/branch
-            let mut supplier_id = supplier.child_seg_by_value(String::from("{}"));
-                supplier_id.query = Some(Query::GETStockSupplierFromId(0));
-                
-                // GET /api/supplier/{id}/name query
-                let mut supplier_id_name = supplier_id.child_seg_by_value(String::from("name"));
-                    supplier_id_name.query = Some(Query::GETStockSupplierNameFromId(0));
+        // GET /api/supplier/{id} query/branch
+        let mut supplier_id = supplier.child_seg_by_value(String::from("{}"));
+            supplier_id.query = Some(Query::GETStockSupplierFromId(0));
+            
+            // GET /api/supplier/{id}/name query
+            let mut supplier_id_name = supplier_id.child_seg_by_value(String::from("name"));
+                supplier_id_name.query = Some(Query::GETStockSupplierNameFromId(0));
 }
 
+
+fn uri_seg_decode(uri: &str) -> String {
+    let mut uri = uri.to_string();
+    let special_regex = Regex::new(r"(%[0-9A-Fa-f]{2})").unwrap();
+    for cap in special_regex.captures_iter(&uri.clone()) {
+        let special = cap.get(1).unwrap().as_str();
+        let special = special.replace("%", "");
+        let special = u8::from_str_radix(&special, 16).unwrap();
+        let special = special as char;
+        uri = uri.replace(cap.get(1).unwrap().as_str(), &special.to_string());
+    }
+    uri
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_uri_seg_decode() {
+        let uri = "/api/supplier/1/Mr%20Smith%20%26%20Co%20Ltd/name";
+        let decoded = super::uri_seg_decode(uri);
+        assert_eq!(decoded, "/api/supplier/1/Mr Smith & Co Ltd/name");
+    }
+}
